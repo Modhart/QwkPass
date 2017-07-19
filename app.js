eb4ef49d5c24366217a761b204aa3dbae66d69f8
Jay <davidw6194@gmail.com>
2017-07-19 20:44:29 +0000

    By Jay - trying a different method for login with firebase

--- app.js
+++ app.js
@@ -1,34 +1,105 @@
-(function() {
-    //Initialize Firebase
-    var config = {
-        apiKey: "AIzaSyBIzEsqTlE5GvUHS1ZBxR8BNDnwGYzfxk4",
-        authDomain: "qwkpass.firebaseapp.com",
-        databaseURL: "https://qwkpass.firebaseio.com",
-        projectId: "qwkpass",
-        storageBucket: "qwkpass.appspot.com",
-        messagingSenderId: "508115255068"
-    };
-    firebase.initializeApp(config);
+var ref = new Firebase("https://qwkpass.firebaseio.com");
 
+// global user (is this a good thing?)
+myUser = -1;
 
-    
-    //Get elements
-    const textemail = document.getElementById('textemail');
-    const textpassword = document.getElementById('textpassword');
-    const btn_login = document.getElementById('btn_login');
-    const btn_signup = document.getElementById('btn_signup');
-    const btn_logout = document.getElementById('btn_logout');
+$(function () {
+    $("#dialog-register").dialog({
+        autoOpen: false,
+        buttons: {
+            "ok": function () {
 
-    //Event Listener
+                var email = $("#register-email").val();
+                var password = $("#register-password").val();
+                authClient.createUser(email, password, function (error, user) {
+                    if (!error) {
+                        console.log('logging new registered user');
+                        doLogin(email, password);
+                    } else {
+                        alert(error);
+                    }
+                });
 
-    btn_login.addEventListener('click', e => {
-        const email = textemail.value;
-        const pass = textpassword.value;
-        const auth = firebase.auth();
-        //Sign in
-        const promise = auth.signInWithEmailAndPassword(email,pass);
-
-        promise.catch (e => console.log(e.message));
-
+                $(this).dialog("close");
+            },
+            Cancel: function () {
+                $(this).dialog("close");
+            }
+        }
     });
-}());
+
+    $("#dialog-login").dialog({
+        autoOpen: false,
+        buttons: {
+            "ok": function () {
+                console.log('trying to login: ' + $("#login-email").val());
+
+                var email = $("#login-email").val();
+                var password = $("#login-password").val();
+
+                doLogin(email, password);
+                $(this).dialog("close");
+            },
+            Cancel: function () {
+                $(this).dialog("close");
+            }
+        }
+    });
+
+    $("#opener-register").click(function () {
+        $("#dialog-register").dialog("open");
+    });
+
+    $("#opener-login").click(function () {
+        $("#dialog-login").dialog("open");
+    });
+
+    $("#opener-logout").click(function () {
+        authClient.logout();
+    });
+});
+
+function doLogin(email, password) {
+    authClient.login('password', {
+        email: email,
+        password: password
+    });
+};
+
+var authClient = new FirebaseAuthClient(ref, function (error, user) {
+    if (error) {
+        alert(error);
+        return;
+    }
+    if (user) {
+        // User is already logged in.
+        console.log('User ID: ' + user.id + ', Provider: ' + user.provider);
+        myUser = user;
+        // doLogin(user);
+        console.log('logged in')
+        $("#data").attr('disabled', false);
+        $("#opener-logout").attr('disabled', false);
+        $("#opener-login").attr('disabled', true);
+    } else {
+        // User is logged out.
+        console.log('logged out');
+        $("#data").attr('disabled', true);
+        $("#opener-logout").attr('disabled', true);
+        $("#opener-login").attr('disabled', false);
+        // ("#dialog-form").dialog("open");
+    }
+});
+
+
+
+$('#data').keypress(function (e) {
+    if (e.keyCode == 13) {
+        var data = $('#data').val();
+        console.log(myUser.id);
+        var myRef = new Firebase("https://xxx.firebaseio.com/users/" + myUser.id);
+        myRef.push({
+            data: data
+        });
+        $('#data').val('');
+    }
+});
