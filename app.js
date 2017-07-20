9b4e2129744e694e01143ef423614b4381c2701c
Jay <davidw6194@gmail.com>
2017-07-20 05:58:06 +0000

    David's attempt to add Firebase -> added Node.Js and other stuff

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-<script src="https://www.gstatic.com/firebasejs/4.1.3/firebase.js"></script>
 <script src="https://www.gstatic.com/firebasejs/4.1.3/firebase-app.js"></script>
+<script src="https://www.gstatic.com/firebasejs/4.1.3/firebase.js"></script>
 <script src="https://www.gstatic.com/firebasejs/4.1.3/firebase-auth.js"></script>
 <script src="https://www.gstatic.com/firebasejs/4.1.3/firebase-database.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
@@ -14,106 +14,144 @@ var config = {
   };
 firebase.initializeApp(config);
 
-// global user (is this a good thing?)
-myUser = -1;
+//Get elements
+const txtEmail = document.getElementById('txtEmail');
+const txtPassword = document.getElementById('txtPassword');
+const btnLogin = document.getElementById('btnLogin');
+const btnSignUp = document.getElementById('btnSignUp');
+const btnLogout = document.getElementById('btnLogout');
 
-$(function () {
-    $("#dialog-register").dialog({
-        autoOpen: false,
-        buttons: {
-            "ok": function () {
-
-                var email = $("#register-email").val();
-                var password = $("#register-password").val();
-                authClient.createUser(email, password, function (error, user) {
-                    if (!error) {
-                        console.log('logging new registered user');
-                        doLogin(email, password);
-                    } else {
-                        alert(error);
-                    }
-                });
-
-                $(this).dialog("close");
-            },
-            Cancel: function () {
-                $(this).dialog("close");
-            }
-        }
-    });
-
-    $("#dialog-login").dialog({
-        autoOpen: false,
-        buttons: {
-            "ok": function () {
-                console.log('trying to login: ' + $("#login-email").val());
-
-                var email = $("#login-email").val();
-                var password = $("#login-password").val();
-
-                doLogin(email, password);
-                $(this).dialog("close");
-            },
-            Cancel: function () {
-                $(this).dialog("close");
-            }
-        }
-    });
-
-    $("#opener-register").click(function () {
-        $("#dialog-register").dialog("open");
-    });
-
-    $("#opener-login").click(function () {
-        $("#dialog-login").dialog("open");
-    });
-
-    $("#opener-logout").click(function () {
-        authClient.logout();
-    });
+//Add Login event
+btnLogin.addEventListener('click', e=> {
+    //Get email and pass
+    const email = txtEmail.value;
+    const pass = txtPassword.value;
+    const auth = firebase.auth();
+    //Sign in
+    const promise = auth.signInWithEmailAndPassword(email, pass);
+    promise.catch(e => console.log(e.message));
 });
 
-function doLogin(email, password) {
-    authClient.login('password', {
-        email: email,
-        password: password
-    });
-};
+btnSignUp.addEventListener('click', e => {
+    //Get email and pass
+    // TODO: Check for real emails
+    const email = txtEmail.value;
+    const pass = txtPassword.value;
+    const auth = firebase.auth();
+    //Sign in
+    const promise = auth.createUserWithEmailAndPassword(email, pass);
+    promise.catch(e => console.log(e.message));
+});
 
-var authClient = new FirebaseAuthClient(ref, function (error, user) {
-    if (error) {
-        alert(error);
-        return;
-    }
-    if (user) {
-        // User is already logged in.
-        console.log('User ID: ' + user.id + ', Provider: ' + user.provider);
-        myUser = user;
-        // doLogin(user);
-        console.log('logged in')
-        $("#data").attr('disabled', false);
-        $("#opener-logout").attr('disabled', false);
-        $("#opener-login").attr('disabled', true);
+// Add a realtime listener
+firebase.auth().onAuthStateChanged(firebaseUser => {
+    if(firebaseUser) {
+        console.log(firebaseUser);
     } else {
-        // User is logged out.
-        console.log('logged out');
-        $("#data").attr('disabled', true);
-        $("#opener-logout").attr('disabled', true);
-        $("#opener-login").attr('disabled', false);
-        // ("#dialog-form").dialog("open");
+        console.log('not logged in');
     }
 });
 
+// global user (is this a good thing?)
+// myUser = -1;
+
+// $(function () {
+//     $("#dialog-register").dialog({
+//         autoOpen: false,
+//         buttons: {
+//             "ok": function () {
+
+//                 var email = $("#register-email").val();
+//                 var password = $("#register-password").val();
+//                 authClient.createUser(email, password, function (error, user) {
+//                     if (!error) {
+//                         console.log('logging new registered user');
+//                         doLogin(email, password);
+//                     } else {
+//                         alert(error);
+//                     }
+//                 });
+
+//                 $(this).dialog("close");
+//             },
+//             Cancel: function () {
+//                 $(this).dialog("close");
+//             }
+//         }
+//     });
+
+//     $("#dialog-login").dialog({
+//         autoOpen: false,
+//         buttons: {
+//             "ok": function () {
+//                 console.log('trying to login: ' + $("#login-email").val());
+
+//                 var email = $("#login-email").val();
+//                 var password = $("#login-password").val();
+
+//                 doLogin(email, password);
+//                 $(this).dialog("close");
+//             },
+//             Cancel: function () {
+//                 $(this).dialog("close");
+//             }
+//         }
+//     });
+
+//     $("#opener-register").click(function () {
+//         $("#dialog-register").dialog("open");
+//     });
+
+//     $("#opener-login").click(function () {
+//         $("#dialog-login").dialog("open");
+//     });
+
+//     $("#opener-logout").click(function () {
+//         authClient.logout();
+//     });
+// });
+
+// function doLogin(email, password) {
+//     authClient.login('password', {
+//         email: email,
+//         password: password
+//     });
+// };
+
+// var authClient = new FirebaseAuthClient(ref, function (error, user) {
+//     if (error) {
+//         alert(error);
+//         return;
+//     }
+//     if (user) {
+//         // User is already logged in.
+//         console.log('User ID: ' + user.id + ', Provider: ' + user.provider);
+//         myUser = user;
+//         // doLogin(user);
+//         console.log('logged in')
+//         $("#data").attr('disabled', false);
+//         $("#opener-logout").attr('disabled', false);
+//         $("#opener-login").attr('disabled', true);
+//     } else {
+//         // User is logged out.
+//         console.log('logged out');
+//         $("#data").attr('disabled', true);
+//         $("#opener-logout").attr('disabled', true);
+//         $("#opener-login").attr('disabled', false);
+//         // ("#dialog-form").dialog("open");
+//     }
+// });
 
 
-$('#data').keypress(function (e) {
-    if (e.keyCode == 13) {
-        var data = $('#data').val();
-        console.log(myUser.id);
-        var myRef = new Firebase("https://qwkpass.firebaseio.com/users/" + myUser.id);
-        myRef.push({
-            data: data
-        });
-        $('#data').val('');
-    }
-});
+
+// $('#data').keypress(function (e) {
+//     if (e.keyCode == 13) {
+//         var data = $('#data').val();
+//         console.log(myUser.id);
+//         var myRef = new Firebase("https://qwkpass.firebaseio.com/users/" + myUser.id);
+//         myRef.push({
+//             data: data
+//         });
+//         $('#data').val('');
+//     }
+// });
