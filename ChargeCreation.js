32dacbc1f2d1720527f81d07b4d61168020d9068
Jay <davidw6194@gmail.com>
2017-09-09 22:48:45 +0000

    By Jay, login verified alert and move to account management

--- ChargeCreation.js
+++ ChargeCreation.js
@@ -1,8 +1,9 @@
 // Set your secret key: remember to change this to your live secret key in production
 // See your keys here: https://dashboard.stripe.com/account/apikeys
+<script src="https://js.stripe.com/v3/"></script>
 var stripe = require("stripe")("sk_test_TdclpRssaEx5YI1mr5sVOdJu");
 
-// Token is created using Stripe.js or Checkout!
+// Token is created using Stripe.js or Checkout
 // Get the payment token ID submitted by the form:
 var token = request.body.stripeToken; // Using Express
 
@@ -13,7 +14,7 @@ stripe.customers.create({
 }).then(function(customer) {
   // YOUR CODE: Save the customer ID and other info in a database for later.
   return stripe.charges.create({
-    amount: 1000,
+    amount: 500,
     currency: "usd",
     customer: customer.id,
     address: "12345 example ave",
